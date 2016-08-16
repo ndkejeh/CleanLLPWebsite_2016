4c62bcdfb24cf0fe01bf627d01eb03932dc64f35
ndkejeh <ndkejeh@gmail.com>
2016-08-16 08:10:46 +0100

    just updating a bit of a dodgy small navbar
    -wql

--- js/LLPSite_2016.js
+++ js/LLPSite_2016.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     var navSmoother = 0;
     var timerVar; 
     var timerFooter;
+    var closeEventTimer; //the timer for the navbar close function
     
     /////////footer script/////////
     var chgFooter = function (){
@@ -28,10 +29,10 @@ $(document).ready(function(){
 
     /////////Navbar script/////////
     $(".nav-wrapper").on("click", function(){
-                           $(this).hide(500);
+                    //       $(this).hide(500);
                            });
 
-    $(".logo-small, .logo-big").mouseover(function() {
+    $(".logo-big").mouseover(function() {
                            $(".nav-wrapper").show(250);
                            if($(window).scrollTop() > 10){//then we'll hide the menu 4 seconds after mouseover
                                 timerVar=setTimeout(function(){$(".nav-wrapper").hide(500)},4000);   
@@ -42,6 +43,25 @@ $(document).ready(function(){
     //need to do an if(open) on scroll hide//
     //nav-content SHOW on the mouseover; HIDE after a few seconds or on scroll//
 
+    $('.logo-small').click(function (){
+        $(".nav-wrapper").toggle(250);
+    });
+    $("a.change-icon").mouseenter(function() {
+                              $(".material-icons").css("color", "#fff");
+                            });      
+    $("a.change-icon").mouseleave(function() {
+//                            $(".material-icons").css("color", "#000");
+                            $(".material-icons").removeAttr("style");
+                            });
+    $("span.material-icons").click(function () {
+                        if($(this).text() == "add") {                    
+                            $(this).next().slideDown("500");
+                            $(this).text("clear");                                                
+                        } else {
+                            $(this).next().slideUp("500");                                                                        
+                            $(this).text("add");
+                        }                    
+                    });
 
     $(window).scroll(function(){
         if($(this).scrollTop()<10){
@@ -55,5 +75,11 @@ $(document).ready(function(){
             $(".nav-wrapper").hide(500);
         }
     });
-
+    
+    //Important close function for navbar//
+    closeEventTimer = setInterval(function() {
+                        if($(".nav-wrapper, .logo-small, .logo-big").is(":hover")){
+                            alert("It's true!!");
+                        };    
+    }, 300);
 });
